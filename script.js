72fa2d050dae8f07127f117f5e6785297ea1cd55
gdemare <44768462+gdemare@users.noreply.github.com>
2019-10-11 15:38:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//----------------------------------------------
-//script add html
 $(document).ready(function() {
     //----------------------------------------------
     // Print the memento menu
@@ -129,59 +127,73 @@ $(document).ready(function() {
             varStyle = false;
         };
     });
-var menFolder = [
-  'company',
-  'company',
-  'company',
-  'database',
-  'database',
-  'database',
-  'english',
-  'microsoft',
-  'microsoft',
-  'microsoft',
-  'programming',
-  'programming',
-  'projects',
-  'unix',
-  'unix',
-  'unix',
-  'unix',
-  'web',
-  'web',
-  'web',
-  'web'
-  ];
-  var menFile = [
-  'animateur',
-  'MOA-MOE',
-  'job-interview',
-  'data-warehouse',
-  'json',
-  'sql',
-  'verbe',
-  'excel',
-  'outlook',
-  'word',
-  'java',
-  'python',
-  'raspberry-wifi',
-  'bash',
-  'folders-linux',
-  'shell',
-  'vim',
-  'css',
-  'html',
-  'js',
-  'php'
-  ];
 
-  //******************************
-  // BEGINING load the content of each page
-  //******************************
-  function loadContenu(idContenu) {
-  var menIdLine = menFile.indexOf(idContenu);
-  var url = 'https://raw.githubusercontent.com/gdemare/aide-memoire/master/' + menFolder[menIdLine] + '/' + menFile[menIdLine] + '.md';
+// dossier ; sous dossier; fichier; general; r; sas; python
+content = [
+['company',null,'animateur','1','0','0','0'],
+['company',null,'MOA-MOE','1','0','0','0'],
+['company',null,'job-interview','1','0','0','0'],
+['database',null,'data-warehouse','1','0','0','0'],
+['database',null,'json','1','0','0','0'],
+['database',null,'sql','1','0','0','0'],
+['english',null,'verbe','1','0','0','0'],
+['microsoft',null,'excel','1','0','0','0'],
+['microsoft',null,'outlook','1','0','0','0'],
+['microsoft',null,'word','1','0','0','0'],
+['programming',null,'java','1','0','0','0'],
+['programming',null,'python','1','0','0','0'],
+['projects',null,'raspberry-wifi','1','0','0','0'],
+['statistique','data-mining','ACM','1','1','1','0'],
+['statistique','data-mining','ACP','1','1','1','0'],
+['statistique','data-mining','AFC','1','1','1','0'],
+['statistique','data-mining','ANOVA','1','1','1','0'],
+['statistique','data-mining','ARMA','1','0','0','0'],
+['statistique','data-mining','CAH','1','1','1','0'],
+['statistique','data-mining','CDH','1','0','0','0'],
+['statistique','data-mining','SVM','1','1','0','0'],
+['statistique','data-mining','analyse-discriminante','1','1','1','0'],
+['statistique','data-mining','arbre-de-decision','1','1','1','0'],
+['statistique','data-mining','boosting','1','0','0','0'],
+['statistique','data-mining','carte-auto-adaptative','1','0','0','0'],
+['statistique','data-mining','centre-mobiles','1','0','1','0'],
+['statistique','data-mining','centroide','1','0','1','0'],
+['statistique','data-mining','classificateur-baysien','1','1','0','0'],
+['statistique','data-mining','cluestering-variables','1','0','1','0'],
+['statistique','data-mining','esperance-maximisation','1','0','1','0'],
+['statistique','data-mining','foret-aleatoire','1','1','0','0'],
+['statistique','data-mining','lissage-exponentiel','1','0','0','0'],
+['statistique','data-mining','perceptron','1','0','0','0'],
+['statistique','data-mining','predire-evaluer','0','1','0','0'],
+['statistique','data-mining','probleme-correlation','1','0','0','0'],
+['statistique','data-mining','regression-lineaire','1','0','1','0'],
+['statistique','data-mining','regression-logistique','1','1','1','0'],
+['statistique','data-mining','reseaux-neurones','1','1','0','0'],
+['statistique','data-mining','serie-temporelle-simple','0','1','0','0'],
+['statistique','data-mining','serie-temporelle','1','0','0','0'],
+['statistique','methode','cluestering-sans-cible','1','0','0','0'],
+['statistique',null,'automatiser','0','1','1','0'],
+['statistique',null,'carte','0','1','0','0'],
+['statistique',null,'classification','1','0','0','0'],
+['statistique',null,'deploy-Rshinny-shinyapps','1','0','0','0'],
+['statistique',null,'dictionnaire-statistique','1','0','0','0'],
+['statistique',null,'formater-donnees','0','1','1','0'],
+['statistique',null,'graphiques','0','1','1','0'],
+['statistique',null,'manipuler-donnees','0','1','1','0'],
+['statistique',null,'reduction-dimensions','1','0','0','0'],
+['statistique',null,'statistique-descriptive','0','1','1','0'],
+['statistique',null,'tableau-de-bord','1','0','0','0'],
+['statistique',null,'tests','1','0','1','0'],
+['unix',null,'bash','1','0','0','0'],
+['unix',null,'folders-linux','1','0','0','0'],
+['unix',null,'shell','1','0','0','0'],
+['unix',null,'vim','1','0','0','0'],
+['web',null,'css','1','0','0','0'],
+['web',null,'html','1','0','0','0'],
+['web',null,'js','1','0','0','0'],
+['web',null,'php','1','0','0','0']];
+
+
+function loadContenu(url) {
   var aPromise = fetch(url);
   // Work with Promise object:
   aPromise
@@ -205,68 +217,92 @@ var menFolder = [
   // Network Error!
   console.log(error);
   });
-  };
-  //******************************
-  // END load the content of each page
-  //******************************
-  for (var i = 0; i <= menFile.length - 1; i++) {
-    var nameA = menFile[i];
-    $( '#' + nameA ).click( function() {
-      idContenu = $(this)[0].id;
-      loadContenu(idContenu);
-    });
-  };
-  //******************************
-  //statistics
-  //******************************
-  // dossier, fichier, general, r, sas
-var sumaryStat = 
-[
-  ['data-mining/', 'ACM', 1, 1, 1],
-  ['data-mining/', 'ACP', 1, 1, 1],
-  ['data-mining/', 'AFC', 1, 1, 1],
-  ['data-mining/', 'ANOVA', 1, 1, 1],
-  ['data-mining/', 'ARMA', 1, 0, 0],
-  ['data-mining/', 'CAH', 1, 1, 1],
-  ['data-mining/', 'CDH', 1, 0, 0],
-  ['data-mining/', 'SVM', 1, 1, 0],
-  ['data-mining/', 'analyse-discriminante', 1, 1, 1],
-  ['data-mining/', 'arbre-de-decision', 1, 1, 1],
-  ['data-mining/', 'boosting', 1, 0, 0],
-  ['data-mining/', 'carte-auto-adaptative', 1, 0, 0],
-  ['data-mining/', 'centre-mobiles', 1, 0, 1],
-  ['data-mining/', 'centroide', 1, 0, 1],
-  ['data-mining/', 'classificateur-baysien', 1, 1, 0],
-  ['data-mining/', 'cluestering-variables', 1, 0, 1],
-  ['data-mining/', 'esperance-maximisation', 1, 0, 1],
-  ['data-mining/', 'foret-aleatoire', 1, 1, 0],
-  ['data-mining/', 'lissage-exponentiel', 1, 0, 0],
-  ['data-mining/', 'perceptron', 1, 0, 0],
-  ['data-mining/', 'predire-evaluer', 0, 1, 0],
-  ['data-mining/', 'probleme-correlation', 1, 0, 0],
-  ['data-mining/', 'regression-lineaire', 1, 0, 1],
-  ['data-mining/', 'regression-logistique', 1, 1, 1],
-  ['data-mining/', 'reseaux-neurones', 1, 1, 0],
-  ['data-mining/', 'serie-temporelle-simple', 0, 1, 0],
-  ['data-mining/', 'serie-temporelle', 1, 0, 0],
-  ['methode/', 'cluestering-sans-cible', 1, 0, 0],
-  ['', 'automatiser', 0, 1, 1],
-  ['', 'carte', 0, 1, 0],
-  ['', 'classification', 1, 0, 0],
-  ['', 'deploy-Rshinny-shinyapps', 1, 0, 0],
-  ['', 'dictionnaire-statistique', 1, 0, 0],
-  ['', 'formater-donnees', 0, 1, 1],
-  ['', 'graphiques', 0, 1, 1],
-  ['', 'manipuler-donnees', 0, 1, 1],
-  ['', 'reduction-dimensions', 1, 0, 0],
-  ['', 'statistique-descriptive', 0, 1, 1],
-  ['', 'tableau-de-bord', 1, 0, 0],
-  ['', 'tests', 1, 0, 1]
-];
+};
 
+function getCol(matrix, col){
+ var column = [];
+ for(var i=0; i<matrix.length; i++){
+    column.push(matrix[i][col]);
+ }
+ return column;
+}
 
-var statLoad = sumaryStat[28];
-console.log( url );
-var url = 'https://raw.githubusercontent.com/gdemare/aide-memoire/master/' + statLoad[0] + statLoad[1] + '.md';
-console.log( url );
+var mementoMenuHtml = "";
+var classe = "";
+
+var menu = new Array();
+for (var i = 0; i <= content.length - 1; i++) {
+  
+  if (i==0) { 
+    condition = true;
+    condition2 = content[i][1] != null;
+  } else { 
+    condition = content[i][0]!=content[i-1][0];
+    condition2 = content[i][1]!=content[i-1][1] && content[i][1]!=null
+  };
+  
+  classe = content[i][0];
+
+  if (condition) {
+    mementoMenuHtml = mementoMenuHtml + "<div id="+content[i][0]+'><i class="fa fa-angle-right"></i>&ensp;'+content[i][0]+"</div>";
+    menu[menu.length] = content[i][0];
+  };
+  if (condition2) {
+    mementoMenuHtml = mementoMenuHtml + '<div class="'+ classe +'" id="' + content[i][0] + content[i][1]+ '" style="display: none;">&ensp;<i class="fa fa-angle-right"></i>&ensp;'+content[i][1]+"</div>";
+    menu[menu.length] = classe + content[i][1];
+  };
+
+  if (content[i][1] !== null ) {
+    classe = classe + content[i][1];
+    libelle = '&ensp;&ensp;&ensp;&ensp;'+content[i][2];
+  } else {
+    libelle = '&ensp;&ensp;&ensp;'+content[i][2];
+  };
+  mementoMenuHtml = mementoMenuHtml + '<div class="'+ classe +'" id=' + content[i][2]+ ' style="display: none;">'+ libelle +"</div>";
+};
+document.getElementById('mementoMenuBox').innerHTML = mementoMenuHtml;
+
+// ouvrir et fermer le menu
+for (var i = 0; i <= menu.length - 1; i++) {
+  $("#" + menu[i]).click(function() {
+    menu = $(this)[0].id;
+    angle = $("#" + menu + " i");
+    if (angle[0].className == 'fa fa-angle-down') {
+      angle.removeClass('fa-angle-down');
+      angle.addClass('fa-angle-right');
+      valeur = 'none';
+    } else {
+      angle.removeClass('fa-angle-right');
+      angle.addClass('fa-angle-down');
+      valeur = 'block';
+    };
+    var elems = document.getElementsByClassName(menu);
+    for (var i=0;i<elems.length;i+=1){
+      if (elems[i].className == menu) {
+        elems[i].style.display = valeur;
+      };
+    };
+  });
+}
+
+// charger les pages
+for (var i = 0; i <= content.length - 1; i++) {
+  $( '#' + content[i][2] ).click( function() {
+    id = getCol(content, 2);
+    var fichier;
+    for (var i = 0; i <= content.length - 1; i++) {
+      if (id[i] == $(this)[0].id) {
+        if (content[i][1] !== null) {
+            fichier = content[i][0] + '/' + content[i][1];
+          } else {
+            fichier = content[i][0]
+          };
+        fichier = fichier + '/' + content[i][2] + '.md';
+      }
+    };
+    url = 'https://raw.githubusercontent.com/gdemare/aide-memoire/master/' + fichier;
+    loadContenu(url);
+  });
+};
+
 });
