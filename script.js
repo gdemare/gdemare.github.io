61c4488f317f86910aa57cf90e762781501ae875
gdemare <44768462+gdemare@users.noreply.github.com>
2021-05-29 11:58:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ $(document).ready(function() {
     root.style.setProperty('--color-gray','rgb(142,142,147)');
     root.style.setProperty('--color-boiteH2','rgb(50,50,54)');
     root.style.setProperty('--color-write','rgb(118,118,119)');
-    root.style.setProperty('--color-bouton','rgb(232,232,232)');
+    root.style.setProperty('--color-bouton','rgb(217,225,225)');
     root.style.setProperty('--color-nav','rgb(247,247,250)');
-    root.style.setProperty('--color-boite','rgb(255,255,255)');
+    root.style.setProperty('--color-boite','rgb(217,225,225)');
     root.style.setProperty('--color-fond','rgb(247,247,250)');
     root.style.setProperty('--color-titre','rgb(180,185,190)');
     root.style.setProperty('--color-degrade1','rgb(0,179,255)');
