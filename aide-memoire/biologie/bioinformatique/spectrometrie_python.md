607163c85660a921f1136b7bed664f05aad51a03
gdemare <44768462+gdemare@users.noreply.github.com>
2022-11-02 08:45:21 +0100

    a

---
+++ aide-memoire/biologie/bioinformatique/spectrometrie_python.md
@@ -0,0 +1,11 @@
+* [Bioconda](https://bioconda.github.io/conda-package_index.html)
+* [pyOpenMS](https://pyopenms.readthedocs.io/en/latest/index.html)
+
+[Expasy](https://www.expasy.org/) bdd Swiss Bioinformatics Ressource Portal :
+	* [UniProt](https://www.uniprot.org/)
+
+Library `Bio` mython 
+
+Alignement de séquences 
+* Le module `AlignIO` de la library `Bio`. 
+* [Peptide SAR](https://github.com/Merck/PepSeA)
--- js/fichiers.js
+++ js/fichiers.js
@@ -1,7 +1,8 @@
 // dossier ; sous dossier; fichier; general; r; sas; python
 donnee = [
-    ['bioinformatique',null,'spectrometrie',0,0,0,1],
+    ['biologie', 'bioinformatique','spectrometrie',0,0,0,1],
     ['biologie',null,'methodes-etudes',1,0,0,0],
+    ['biologie', null, 'fidji', 1, 0, 0, 0],
     ['biologie', null, 'methodes-observation', 1, 0, 0, 0],
     ['company',null,'animateur',1,0,0,0],
     ['company', null, 'job-interview', 1, 0, 0, 0],
